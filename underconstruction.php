84947d367803d06243921b876f27d5d37a7a7fc4
gameover98 <farulg@gmail.com>
2023-03-31 11:57:24 +0800

    Update underconstruction.php

--- underconstruction.php
+++ underconstruction.php
@@ -2,23 +2,36 @@
 <html>
 <head>
 	<title>Under Construction</title>
+	<style>
+		body {
+			background-color: <?php echo generateRandomPastelColor(); ?>;
+		}
+	</style>
 </head>
 <body>
 	<h1>Under Construction</h1>
-	<p>Sorry, this website is currently under construction. We're working hard to bring you something awesome!</p>
-	<?php 
-	// Generate a random construction joke
-	$jokes = array(
-		"Why did the construction worker quit his job? He didn't get enough concrete evidence.",
-		"Why was the math book sad? Because it had too many problems.",
-		"Why did the tomato turn red? Because it saw the salad dressing!",
-		"What did the grape say when it got stepped on? Nothing, it just let out a little wine.",
-		"Why did the bicycle fall over? Because it was two tired."
-	);
-	$random_joke = $jokes[array_rand($jokes)];
-	?>
-	<p>Here's a joke to make your wait a little more bearable:</p>
-	<blockquote><?php echo $random_joke; ?></blockquote>
+	<p>This QB website is currently under construction. Please check back later.</p>
+	<p>Hostname: <?php echo gethostname(); ?></p>
+	<p><?php echo getJoke(); ?></p>
 </body>
 </html>
 
+<?php
+function generateRandomPastelColor() {
+    $r = mt_rand(100, 255);
+    $g = mt_rand(100, 255);
+    $b = mt_rand(100, 255);
+    return "rgb(" . $r . "," . $g . "," . $b . ", 0.8)";
+}
+
+function getJoke() {
+    $jokes = array(
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "What did the grape say when it got stepped on? Nothing, it just let out a little wine.",
+        "Why was the math book sad? Because it had too many problems.",
+        "What do you call a fish wearing a bowtie? Sofishticated.",
+        "I'm reading a book on the history of glue. I just can't seem to put it down."
+    );
+    return $jokes[array_rand($jokes)];
+}
+?>
